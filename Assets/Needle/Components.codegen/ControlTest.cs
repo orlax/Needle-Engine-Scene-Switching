c4aa25c54db9e87967ecfdcd9c00b8f332233620
Orlando Almario <orlax22@gmail.com>
2023-03-15 12:24:27 -0500

    comiiting changes with a test controller and some physics problems

---
+++ Assets/Needle/Components.codegen/ControlTest.cs
@@ -0,0 +1,20 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class ControlTest : UnityEngine.MonoBehaviour
+	{
+		public float @moveSpeed = 3f;
+		public float @vertical = 0f;
+		public float @horizontal = 0f;
+		public UnityEngine.Vector3 @speed = new UnityEngine.Vector3();
+		public void update(){}
+		public void CheckOnlyForward(){}
+		public void CheckWithRays(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
